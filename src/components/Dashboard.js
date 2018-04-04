8759c087b499091f6e0fbebf8499fbeee202107e
mwinslow331 <michael.winslow331@gmail.com>
2018-04-04 14:01:34 -0400

    added react router and updated poll links using routers Link

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 class Dashboard extends Component {
@@ -44,7 +45,11 @@ class Dashboard extends Component {
         </div>
         <ul className='dashboard-list'>
           {list.map((poll) => (
-            <li key={poll.id}>{poll.question}</li>
+            <li key={poll.id}>
+              <Link to={`polls/${poll.id}`}>
+                {poll.question}
+              </Link>
+            </li>
           ))}
         </ul>
       </div>
